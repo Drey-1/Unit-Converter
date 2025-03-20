976019ee314218705b31ddbba94439302e7dadb3
Drey-1 <matosandrey11@gmail.com>
2025-03-20 17:10:47 -0300

    integrate temperature.js on server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const temperatureRoute = require("./temperature.js")
 
 app.use(express.json());
+app.use("/",temperatureRoute);
 
 
 module.exports = app
