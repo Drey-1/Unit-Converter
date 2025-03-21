da23095484566e54699ab9b6543bfe744f018160
Drey-1 <matosandrey11@gmail.com>
2025-03-21 16:17:07 -0300

    route weight created and integrated to server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const temperatureRoute = require("./temperature.js")
+const weightRoute = require("./weight.js")
 
 app.use(express.json());
 app.use("/",temperatureRoute);
-
+app.use("/",weightRoute);
 
 module.exports = app
--- BackEnd/weight.js
+++ BackEnd/weight.js
@@ -1,8 +1,70 @@
 const express = require("express");
 const weight = express.Router();
- 
-function MgtoG(value){
 
+//----------------------- Mg to anyone functions
+function MgToG(value){
+    return value / 1000;
+}
+function MgToKg(value){
+    return MgToG(value) / 1000;
+}
+function MgToO(value){
+    return MgToKg(value) * 35.274;
+}
+function MgToP(value){
+    return MgToKg(value) * 2.20462;
+}
+//----------------------- G to anyone functions
+function GToMg(value){
+    return value * 1000;
+}
+function GToKg(value){
+    return value / 1000;
+}
+function GToO(value){
+    return value / 28.35;
+}
+function GToP(value){
+    return value / 453.592;
+}
+//----------------------- Kg to anyone functions
+function KgToMg(value){
+    return KgToG(value) * 1000;
+}
+function KgToG(value){
+    return value * 1000;
+}
+function KgToO(value){
+    return value * 35.274;
+}
+function KgToP(value){
+    return value * 2.20462;
+}
+//----------------------- O to anyone functions
+function OToMg(value){
+    return OToG(value) * 1000;
+}
+function OToG(value){
+    return value * 28.35;
+}
+function OToKg(value){
+    return OToG(value) / 1000;
+}
+function OToP(value){
+    return value / 16;
+}
+//----------------------- P to anyone functions
+function PToMg(value){
+    return PToG(value) * 1000;
+}
+function PToG(value){
+    return value * 453.59237;
+}
+function PToKg(value){
+    return PToG(value) / 1000;
+}
+function PToO(value){
+    return value * 16;
 }
 
 weight.post("/weight",(req,res) => {
@@ -75,5 +137,7 @@ weight.post("/weight",(req,res) => {
         default:
             return res.status(400).json({ error: "Conversion not supported" });
     }
-    return res.json({ convertedValue })
+    return res.json({ convertedValue });
 })
+
+module.exports = weight;
