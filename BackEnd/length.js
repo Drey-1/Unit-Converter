19cfcea0ffe010641eda22ede39d5195d6f0d07e
Drey-1 <matosandrey11@gmail.com>
2025-03-22 19:25:04 -0300

    Route /length refatored

--- BackEnd/length.js
+++ BackEnd/length.js
@@ -112,88 +112,147 @@ function IToY(value){
 function IToMi(value){
      return value / 63360; 
 }
+//----------------------- F to anyone functions
+function FToMm(value) {
+     return value * 304.8;
+}
+ function FToCm(value) {
+     return value * 30.48;
+}
+ function FToM(value) {
+     return value * 0.3048;
+}
+function FToKm(value) {
+     return value * 0.0003048;
+}
+ function FToI(value) {
+     return value * 12;
+}
+ function FToY(value) {
+     return value / 3; 
+}
+ function FToMi(value) {
+     return value / 5280; 
+}
+//----------------------- Y to anyone functions
+function YToMm(value) {
+     return value * 914.4;
+}
+function YToCm(value) {
+     return value * 91.44;
+}
+function YToM(value) {
+     return value * 0.9144;
+}
+function YToKm(value) {
+     return value * 0.0009144;
+}
+function YToI(value) {
+     return value * 36;
+}
+function YToF(value) {
+     return value * 3;
+}
+function YToMi(value) {
+     return value / 1760;
+}
+//----------------------- Mi to anyone functions
+function MiToMm(value) {
+     return value * 1609344;
+}
+function MiToCm(value) {
+     return value * 160934.4;
+}
+function MiToM(value) {
+     return value * 1609.344;
+}
+function MiToKm(value) {
+     return value * 1.609344;
+}
+function MiToI(value) {
+     return value * 63360;
+}
+function MiToF(value) {
+     return value * 5280;
+}
+function MiToY(value) {
+     return value * 1760;
+}
 
 length.post("/length", (req, res) => {
     let { value, fromUnit, toUnit } = req.body;
-    let convertedValue;
+    
     if (fromUnit === toUnit) {
         return res.json({ convertedValue: value });
     }
-    switch (`${fromUnit}-${toUnit}`) {
-        case "Mm-Cm":
-             convertedValue = MmToCm(value); break;
-        case "Mm-M":
-             convertedValue = MmToM(value); break;
-        case "Mm-Km":
-             convertedValue = MmToKm(value); break;
-        case "Mm-I":
-             convertedValue = MmToI(value); break;
-        case "Mm-F":
-             convertedValue = MmToF(value); break;
-        case "Mm-Y":
-             convertedValue = MmToY(value); break;
-        case "Mm-Mi":
-             convertedValue = MmToMi(value); break;
-        case "Cm-Mm":
-             convertedValue = CmToMm(value); break;
-        case "Cm-M":
-             convertedValue = CmToM(value); break;
-        case "Cm-Km":
-             convertedValue = CmToKm(value); break;
-        case "Cm-I":
-             convertedValue = CmToI(value); break;
-        case "Cm-F":
-             convertedValue = CmToF(value); break;
-        case "Cm-Y":
-             convertedValue = CmToY(value); break;
-        case "Cm-Mi":
-             convertedValue = CmToMi(value); break;
-        case "M-Mm":
-             convertedValue = MToMm(value); break;
-        case "M-Cm":
-             convertedValue = MToCm(value); break;
-        case "M-Km":
-             convertedValue = MToKm(value); break;
-        case "M-I":
-             convertedValue = MToI(value); break;
-        case "M-F":
-             convertedValue = MToF(value); break;
-        case "M-Y":
-             convertedValue = MToY(value); break;
-        case "M-Mi":
-             convertedValue = MToMi(value); break;
-        case "Km-Mm":
-             convertedValue = KmToMm(value); break;
-        case "Km-Cm":
-             convertedValue = KmToCm(value); break;
-        case "Km-M":
-             convertedValue = KmToM(value); break;
-        case "Km-I":
-             convertedValue = KmToI(value); break;
-        case "Km-F":
-             convertedValue = KmToF(value); break;
-        case "Km-Y":
-             convertedValue = KmToY(value); break;
-        case "Km-Mi":
-             convertedValue = KmToMi(value); break;
-        case "I-Mm":
-             convertedValue = IToMm(value); break;
-        case "I-Cm":
-             convertedValue = IToCm(value); break;
-        case "I-M":
-             convertedValue = IToM(value); break;
-        case "I-Km":
-             convertedValue = IToKm(value); break;
-        case "I-F":
-             convertedValue = IToF(value); break;
-        case "I-Y":
-             convertedValue = IToY(value); break;
-        case "I-Mi":
-             convertedValue = IToMi(value); break;
-        default:
-            return res.status(400).json({ error: "Conversion not supported" });
+
+    const converterMap = {
+        "Mm-Cm": MmToCm,
+        "Mm-M": MmToM,
+        "Mm-Km": MmToKm,
+        "Mm-I": MmToI,
+        "Mm-F": MmToF,
+        "Mm-Y": MmToY,
+        "Mm-Mi": MmToMi,
+        "Cm-Mm": CmToMm,
+        "Cm-M": CmToM,
+        "Cm-Km": CmToKm,
+        "Cm-I": CmToI,
+        "Cm-F": CmToF,
+        "Cm-Y": CmToY,
+        "Cm-Mi": CmToMi,
+        "M-Mm": MToMm,
+        "M-Cm": MToCm,
+        "M-Km": MToKm,
+        "M-I": MToI,
+        "M-F": MToF,
+        "M-Y": MToY,
+        "M-Mi": MToMi,
+        "Km-Mm": KmToMm,
+        "Km-Cm": KmToCm,
+        "Km-M": KmToM,
+        "Km-I": KmToI,
+        "Km-F": KmToF,
+        "Km-Y": KmToY,
+        "Km-Mi": KmToMi,
+        "I-Cm": IToCm,
+        "I-M": IToM,
+        "I-Km": IToKm,
+        "I-F": IToF,
+        "I-Y": IToY,
+        "I-Mi": IToMi,
+        "I-Mm": IToMm,
+        "F-Mm": FToMm,
+        "F-Cm": FToCm,
+        "F-M": FToM,
+        "F-Km": FToKm,
+        "F-I": FToI,
+        "F-Y": FToY,
+        "F-Mi": FToMi,
+        "Y-Mm": YToMm,
+        "Y-Cm": YToCm,
+        "Y-M": YToM,
+        "Y-Km": YToKm,
+        "Y-I": YToI,
+        "Y-F": YToF,
+        "Y-Mi": YToMi,
+        "Mi-Mm": MiToMm,
+        "Mi-Cm": MiToCm,
+        "Mi-M": MiToM,
+        "Mi-Km": MiToKm,
+        "Mi-I": MiToI,
+        "Mi-F": MiToF,
+        "Mi-Y": MiToY,
+    };
+
+    const key = `${fromUnit}-${toUnit}`;
+    const conversionFunc = converterMap[key];
+
+    if (conversionFunc) {
+        return res.json({ convertedValue: conversionFunc(value) });
+    } else {
+        return res.status(400).json({ error: "Conversion not supported" });
     }
-    return res.json({ convertedValue });
 });
 
 module.exports = length;
