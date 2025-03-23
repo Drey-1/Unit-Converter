88ac188a74798e0459d55b67929aa2cc6761c83d
Drey-1 <matosandrey11@gmail.com>
2025-03-23 15:23:30 -0300

    Route /temperature refatored

--- BackEnd/temperature.js
+++ BackEnd/temperature.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const temperature = express.Router();
 
-function CtoF(value){
+function CToF(value){
     return ((value * 9 / 5) + 32);
 }
 
-function CtoK(value){
+function CToK(value){
     return value + 273.15;
 }
 
-function FtoC(value){
+function FToC(value){
     return ((value - 32) * 5 / 9);
 }
 
-function FtoK(value){
+function FToK(value){
     return FtoC(value) + 273.15;
 }
 
-function KtoC(value){
+function KToC(value){
     return value - 273.15;
 }
 
-function KtoF(value){
+function KToF(value){
     return CtoF(KtoC(value));
 }
 
@@ -30,30 +30,22 @@ temperature.post("/temperature",(req,res) => {
     if(fromUnit === toUnit){
         return res.json({ convertedValue : value });
     }
-    switch (`${fromUnit}-${toUnit}`){
-        case "C-F":
-            convertedValue = CtoF(value);
-            break
-        case "C-K":
-            convertedValue = CtoK(value);
-            break
-        case "F-C":
-            convertedValue = FtoC(value);
-            break
-        case "F-K":
-            convertedValue = FtoK(value);
-            break
-        case "K-C":
-            convertedValue = KtoC(value);
-            break
-        case "K-F":
-            convertedValue = KtoF(value);
-            break
-        default:
-            return res.status(400).json({ error: "Conversion not supported" }); 
+    const converterMap = {
+        "C-F": CToF,
+        "C-K": CToK,
+        "F-C": FToC,
+        "F-K": FToK,
+        "K-C": KToC,
+        "K-F": KToF,
     }
-
-    return res.json({ convertedValue });
+    const key = `${fromUnit}-${toUnit}`;
+    const conversionFunc = converterMap[key];
+    if(conversionFunc){
+        return res.json({ convertedValue: conversionFunc(value)})
+    } else {
+        return res.status(400).json({ error: "Conversion not supported" });
+    }
+    
 });
 
 module.exports = temperature;
